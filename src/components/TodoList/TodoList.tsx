ddd1e97ea475e1a65fd938b65558ac836a9f8fde
Manthan Kuber <manthankuber873@gmail.com>
2022-04-24 09:59:44 +0530

    feat: filter by status (buggy)

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -7,8 +7,21 @@ const TodoList = () => {
   const dispatch = useAppDispatch();
   return (
     <StyledUl>
-      {todo.selectedTab === "All"
-        ? todo.todoList.map((item,index) => (
+      {todo.selectedTab === "All" ? (
+        todo.todoList.map((item, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={item.status}
+              onChange={() => dispatch(checkUncheck(index))}
+            />
+            <p className={item.status ? "strikedTodo" : ""}>{item.name}</p>
+          </li>
+        ))
+      ) : todo.selectedTab === "Active" ? (
+        todo.todoList
+          .filter((item) => item.status === false)
+          .map((item, index) => (
             <li key={index}>
               <input
                 type="checkbox"
@@ -18,7 +31,22 @@ const TodoList = () => {
               <p className={item.status ? "strikedTodo" : ""}>{item.name}</p>
             </li>
           ))
-        : ""}
+      ) : todo.selectedTab === "Completed" ? (
+        todo.todoList
+          .filter((item) => item.status === true)
+          .map((item, index) => (
+            <li key={index}>
+              <input
+                type="checkbox"
+                checked={item.status}
+                onChange={() => dispatch(checkUncheck(index))}
+              />
+              <p className={item.status ? "strikedTodo" : ""}>{item.name}</p>
+            </li>
+          ))
+      ) : (
+        <></>
+      )}
     </StyledUl>
   );
 };
